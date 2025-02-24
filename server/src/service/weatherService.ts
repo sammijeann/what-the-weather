fab27ac8a959965d632db272db1dfb623ae66c48
Sammi Teel <sammijteel@gmail.com>
2025-02-24 11:47:53 -0600

    set-up basics completed

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,8 +2,31 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
 
 // TODO: Define a class for the Weather object
+class Weather {
+  tempF: number;
+  humidity: number;
+  windSpeed: number;
+  icon: string;
+  date?: string;
+  city?: string;
+  iconDescription?: string;
+
+  constructor(){
+    this.tempF = 0;
+    this.humidity = 0;
+    this.windSpeed = 0;
+    this.icon = '';
+    this.date = '';
+    this.city = '';
+    this.iconDescription = '';
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
@@ -14,8 +37,8 @@ class WeatherService {
   cityName: string; 
 
   constructor() {
-    this.baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={weatherApi}`;
-    this.weatherApi = '7b46fa542a492cddebfdf753a9695a55';
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.weatherApi = process.env.API_KEY || '';
     this.cityName = ''; // This will be set when you create an instance of the class
 }
 
@@ -25,13 +48,22 @@ class WeatherService {
   }
 
   // TODO: Create destructureLocationData method
-  private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    
+    return { lat: 0, lon: 0 };
+  }
 
   // TODO: Create buildGeocodeQuery method
-  private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string {
+
+    return '';
+  }
 
   // TODO: Create buildWeatherQuery method
-  private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates): string {
+
+    return '';
+  }
 
   // TODO: Create fetchAndDestructureLocationData method
   private async fetchAndDestructureLocationData() {}
